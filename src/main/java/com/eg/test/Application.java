097221e6d2f6e89e3ccd395c2c373d52c4345c6e
shift2C1 <48273530+shift2C1@users.noreply.github.com>
2019-08-20 20:55:06 +0800

    持久层

--- src/main/java/com/eg/test/Application.java
+++ src/main/java/com/eg/test/Application.java
@@ -1,9 +1,11 @@
 package com.eg.test;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.eg.test.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
--- src/main/java/com/eg/test/controller/TestController.java
+++ src/main/java/com/eg/test/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.eg.test.controller;
 
+import com.eg.test.mapper.AreaMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 @Slf4j
 public class TestController {
+    /**
+     *
+     */
+    @Autowired
+    private AreaMapper areaMapper;
     @RequestMapping("/test")
     public String test(){
         log.info("coming..........");
-        return "success";
+        return areaMapper.selectByExample(null).toString();
+//        return "success";
     }
 }
